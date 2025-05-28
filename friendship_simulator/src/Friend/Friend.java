667a771b692e7caeb58ff1192c0076b320ac1773
Benjamin Andreas Vraspillai <benjamin.vraspillai@protonmail.com>
2025-05-28 17:30:07 +0200

    Made some changes to the friend class.

--- friendship_simulator/src/Friend/Friend.java
+++ friendship_simulator/src/Friend/Friend.java
@@ -2,4 +2,20 @@ package Friend;
 
 public class Friend {
     
+    String friendName;
+    int friendAge;
+
+    public Friend(String friendName, int friendAge) {
+        this.friendName = friendName;
+        this.friendAge = friendAge;
+    }
+
+    public String getFriendName() {
+        return friendName;
+    }
+
+    public int getFriendAge() {
+        return friendAge;
+    }
+    
 }
