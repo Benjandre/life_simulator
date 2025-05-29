48bb8aff829061c84423b5de058ccd15a519dfc4
Benjamin Andreas Vraspillai <benjamin.vraspillai@protonmail.com>
2025-05-29 19:45:37 +0200

    Made some changes.

--- friendship_simulator/src/gameAssets/Places_Rooms_Items/Rooms/Home/Livingroom/Livingroom.java
+++ friendship_simulator/src/gameAssets/Places_Rooms_Items/Rooms/Home/Livingroom/Livingroom.java
@@ -3,5 +3,11 @@ package gameAssets.Places_Rooms_Items.Rooms.Home.Livingroom;
 import gameAssets.Places_Rooms_Items.Rooms.Home.Room;
 
 public class Livingroom extends Room {
+
+    public Livingroom(String roomName, int roomLength, int roomHeight) {
+        super(roomName, roomLength, roomHeight);
+    }
+
+    // Additional methods specific to Livingroom can be added here
     
 }
