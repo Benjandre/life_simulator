e026a6d0df57de2ed9a1a0072c3b9b454d0df1e3
Benjamin Andreas Vraspillai <benjamin.vraspillai@protonmail.com>
2025-05-31 04:47:46 +0200

    Made some changes.

--- friendship_simulator/src/gameAssets/Places_Rooms_Items/Rooms/Home/Kitchen/Kitchen.java
+++ friendship_simulator/src/gameAssets/Places_Rooms_Items/Rooms/Home/Kitchen/Kitchen.java
@@ -1,5 +1,15 @@
 package gameAssets.Places_Rooms_Items.Rooms.Home.Kitchen;
 
-public class Kitchen {
+public class Kitchen extends Room {
+
+    public Kitchen(String roomName, int roomLength, int roomHeight) {
+        super(roomName, roomLength, roomHeight);
+    }
+
+    // Additional methods specific to Kitchen can be added here
+    // For example, methods to manage kitchen appliances or cooking items can be implemented
+    public void cook(String dish) {
+        System.out.println("Cooking " + dish + " in the kitchen.");
+    }
     
 }
