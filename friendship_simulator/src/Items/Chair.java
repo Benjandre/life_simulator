e23011dc8d9c6fb266974ca3d3da25394cc10511
Benjandre <benjamin.vraspillai@protonmail.com>
2025-07-04 14:37:35 +0200

    Made some changes to the "chair" class.

--- friendship_simulator/src/Items/Chair.java
+++ friendship_simulator/src/Items/Chair.java
@@ -8,7 +8,9 @@ public class Chair {
     public Player player;
     public Room room;
 
-    public Chair(Player player) {
+    public Chair(Player player, Room room) {
+        this.player = player;
+        this.room = room;
     }
 
     public Player getPlayer() {
