68513a8bee498e5176c9ae96f343008d7c88fa62
Benjandre <benjamin.vraspillai@protonmail.com>
2025-07-06 04:01:46 +0200

    Made some changes to the "inventory" class.

--- friendship_simulator/src/GameMechanics/Inventory.java
+++ friendship_simulator/src/GameMechanics/Inventory.java
@@ -13,19 +13,42 @@ public class Inventory {
         this.items = new ArrayList<>();
     }
 
-    public void addItem(Item item) {
-        items.add(item);
-    }
-
-    public void removeItem(Item item) {
-        items.remove(item);
-    }
-
     public ArrayList<Item> getItems() {
         return items;
     }
 
     public boolean hasItem(Item item) {
         return items.contains(item);
+    }
+
+    public int getItemAmount() {
+        return items.size();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+        if (item.getItemType().toString().toLowerCase().startsWith("a")) {
+            System.out.println("You added an " + item.getItemType() + " to your inventory.");
+        } else {
+            System.out.println("You added a " + item.getItemType() + " to your inventory.");
+        }
+    }
+
+    public void removeItem(Item item) {
+        items.remove(item);
+        if (item.getItemType().toString().toLowerCase().startsWith("a")) {
+            System.out.println("You removed an " + item.getItemType().toString() + " from your inventory.");
+        } else {
+            System.out.println("You removed a " + item.getItemType().toString() + " from your inventory.");
+        }
+    }
+
+    public void clearInventory() {
+        if (items.isEmpty()) {
+            System.out.println("You can't clear your inventory, since you don't have any items.");
+        } else {
+            items.clear();
+            System.out.println("You cleared your inventory.");
+        }
     }
 }
