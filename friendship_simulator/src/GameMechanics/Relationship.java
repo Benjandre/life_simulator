183bdcaa4f9d558ae6342a4cd84c50cbeb43728a
Benjandre <benjamin.vraspillai@protonmail.com>
2025-07-06 04:17:03 +0200

    Made some changes to the "relationship" class.

--- friendship_simulator/src/GameMechanics/Relationship.java
+++ friendship_simulator/src/GameMechanics/Relationship.java
@@ -1,5 +1,27 @@
 package GameMechanics;
 
+import Models.Player;
+
 public class Relationship {
+
+    private enum relationshipType {
+        STRANGER,
+        ACQUAINTANCE,
+        FRIEND,
+        FAMILY_MEMBER,
+        ROMANTIC_PARTNER
+    }
+
+    private relationshipType relationshipType;
+    private int relationshipLevel;
+
+    public Relationship (relationshipType relationshipType, int relationshipLevel) {
+        this.relationshipType = relationshipType;
+        this.relationshipLevel = relationshipLevel;
+    }
+
+    public void strengthenRelationship() {
+        relationshipLevel++;
+    }
     
 }
