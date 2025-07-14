906a7a3bf6127975f0bed8577bff16aad836e632
Benjandre <benjamin.vraspillai@protonmail.com>
2025-07-14 15:52:17 +0200

    Made some changes.

--- friendship_simulator/src/Main.java
+++ friendship_simulator/src/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        mainMenu mainMenu = new mainMenu("Main Menu", "Start Game", "Options", "Exit Game");
-        mainMenu.loadMainMenu();
-
     }
     
 }
