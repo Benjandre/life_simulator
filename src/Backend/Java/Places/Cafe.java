8923f704295133ec016139d38a652b4d0c1de177
Benjandre <benjamin.vraspillai@protonmail.com>
2026-03-10 21:08:14 +0100

    Made some changes.

--- src/Backend/Java/Places/Cafe.java
+++ src/Backend/Java/Places/Cafe.java
@@ -1,22 +1,37 @@
 package Backend.Java.Places;
 
 import Backend.Java.Models.Place;
+import Backend.Java.Models.Room;
+import Backend.Java.Items.Table;
+import Backend.Java.Models.Character;
+import Backend.Java.Items.Chair;
 
 public class Cafe extends Place {
+    
+    boolean isOpen;
+    private Character worker;
+    private Room room;
+    private Table table;
+    private Chair chair;
+
 
     public Cafe() {
-        super("Cafe", "A cozy place to relax and enjoy a cup of coffee.", "Public");
+        super("Cafe", "A cafe.", Place.LocationType.CAFE);
     }
 
-    @Override
-    public String toString() {
-        return "Cafe{" +
-                "name='" + getName() + '\'' +
-                ", description='" + getDescription() + '\'' +
-                ", locationType='" + getLocationType() + '\'' +
-                '}';
+    public void OpenCafe() {
+        if (isOpen == true) {
+            System.out.println("The cafe is already open.");
+            return;
+        } else {
+            isOpen = true;
+            System.out.println("The cafe is now open.");
+        }
     }
 
+    public void CloseCafe() {
+        isOpen = false;
+    }
 
     
 }
